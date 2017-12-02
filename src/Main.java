27b69a28d9a1bff7bf13437f29ace5a5b7c30e1c
Georgi Gospodinov <georgigospodinov@gmail.com>
2017-12-02 17:56:03 +0000

    Image Loading is now in a separate function before main loop.

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
+
 public class Main {
-    public static final int MAX_SPAWN = 30;
     public static final int X = 2000;
     public static final int Y = 1000;
     public static final double GRAVITY = 1000;
@@ -26,23 +27,20 @@ public class Main {
     public static final double INIT_ARROW_DX = 1000;
     public static final double INIT_ARR_DY = 1500;
 
-    private static JFrame frame;
-    private static Canvas canvas;
     public static BufferStrategy bufferStrategy;
-    private static GraphicsEnvironment graphicsEnvironment;
-    private static GraphicsDevice graphicsDevice;
-    private static GraphicsConfiguration graphicsConfiguration;
     private static BufferedImage bufferedImage;
     private static Graphics graphics;
     private static Graphics2D g2d;
-    private static AffineTransform affineTransform;
     public static ArrayList<Spawn> objects = new ArrayList<Spawn>();
     public static boolean isRunning = true;
     public static final double UPA = 1500;
 
+    private static final HashMap<TYPE, BufferedImage> images = new HashMap<>();
+
     public static void main(String[] args) {
         // Initialize some things.
         initializeJFrame();
+        loadAssets();
         // Create and start threads.
         Thread moveEngine = new MoveEngine();
         moveEngine.start();
@@ -50,35 +48,41 @@ public class Main {
         runAnimation();
     }
 
+    private static void loadAssets() {
+        String path = "assets/img/";
+        for (TYPE t : TYPE.values()) {
+            try {
+                images.put(t, ImageIO.read(new File(path + t.toString() + ".png")));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                System.out.println(t.toString());
+                System.exit(-1);
+            }
+        }
+    }
+
+    private static void drawObjects() {
+        for (Spawn s : objects) {
+            AffineTransform affineTransform = new AffineTransform();
+
+            BufferedImage img = images.get(s.type);
+            double iw2 = img.getWidth() / 2, ih2 = img.getHeight() / 2;
+            affineTransform.translate(s.getX() - iw2, s.getY() - ih2);
+            affineTransform.rotate(s.rotation.angle(), iw2, ih2);
+            g2d.drawRenderedImage(img, affineTransform);
+        }
+    }
+
     public static void runAnimation() {
 
-        String path = "assets/img/";
-
-        //TODO load in memory
-        String imgName = "dragon2.png";
-        BufferedImage img;
-        try {
-            img = ImageIO.read(new File(path + imgName));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-//         Start the loop.
         while (isRunning) {
             try {
                 // clear back bufferedImage...
                 g2d = bufferedImage.createGraphics();
                 g2d.setColor(Color.WHITE);
                 g2d.fillRect(0, 0, X, Y);
-                // Draw entities
-                for (Spawn s : objects) {
-                    affineTransform = new AffineTransform();
-                    double iw2 = img.getWidth()/2, ih2 = img.getHeight()/2;
-                    affineTransform.translate(s.getX()-iw2, s.getY()-ih2);
-                    affineTransform.rotate(s.rotation.angle(), iw2, ih2);
-                    g2d.drawRenderedImage(img, affineTransform);
-                }
+                drawObjects();
                 // Blit image and flip...
                 graphics = bufferStrategy.getDrawGraphics();
                 graphics.drawImage(bufferedImage, 0, 0, null);
@@ -87,7 +91,7 @@ public class Main {
                 Thread.sleep(15);
             }
             //<editor-fold desc="catch-finally">
-            catch (InterruptedException e) {
+            catch (InterruptedException ignored) {
             }
             finally {
                 // release resources
@@ -100,7 +104,7 @@ public class Main {
 
     private static void initializeJFrame() {
         // Create the frame...
-        frame = new JFrame(TITLE);
+        JFrame frame = new JFrame(TITLE);
         frame.addKeyListener(new Keys());
         frame.setFocusable(true);
         frame.setFocusTraversalKeysEnabled(false);
@@ -108,7 +112,7 @@ public class Main {
         frame.setIgnoreRepaint(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // Create canvas for painting...
-        canvas = new Canvas();
+        Canvas canvas = new Canvas();
         canvas.setIgnoreRepaint(true);
         canvas.setSize(X, Y);
         // Add the canvas, and display.
@@ -121,9 +125,9 @@ public class Main {
         canvas.createBufferStrategy(2);
         bufferStrategy = canvas.getBufferStrategy();
         // Get graphics configuration...
-        graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
-        graphicsConfiguration = graphicsDevice.getDefaultConfiguration();
+        GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice graphicsDevice = graphicsEnvironment.getDefaultScreenDevice();
+        GraphicsConfiguration graphicsConfiguration = graphicsDevice.getDefaultConfiguration();
         // Create off-screen drawing surface
         bufferedImage = graphicsConfiguration.createCompatibleImage(X, Y);
         // Objects needed for rendering...
