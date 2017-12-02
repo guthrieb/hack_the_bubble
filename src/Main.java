e000976a616a73835996e36c9301e65246549165
jg229 <joeguthrie2@hotmail.com>
2017-12-02 11:45:44 +0000

    Adding Engine

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static final double GRAVITY = 1500;
+    public static final double BOUNCE = 100;
+    static ArrayList<Spawn> objects = new ArrayList<Spawn>();
+    static boolean isRunning;
+
+    public static void main(String args[]){
+
+
+
+
+    }
+
+
 }
